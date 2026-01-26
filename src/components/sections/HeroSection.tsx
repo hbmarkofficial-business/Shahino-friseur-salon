85d674aa8b0c7c75d9a82a6bb660727d4da82ed0
hbmarkofficial-business <hbmark.official@gmail.com>
2026-01-26 11:21:15 +0100

    Update HeroSection.tsx

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -40,8 +40,8 @@ export const HeroSection = () => {
             className="font-display text-5xl md:text-7xl lg:text-8xl font-bold text-foreground leading-tight opacity-0 animate-fade-up"
             style={{ animationDelay: '400ms', animationFillMode: 'forwards' }}
           >
-            Lord Barbershop
-            <span className="block text-gradient">Coiffeur</span>
+            Shahino
+            <span className="block text-gradient">Friseursalon</span>
           </h1>
 
           {/* Description */}
